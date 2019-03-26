c03570d1e36a8fd30f13815167bcda297767afb7
loladeking <loladeking@gmail.com>
2019-03-26 17:44:06 +0100

    Add project files.

---
+++ MultiwarehouseMgt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiwarehouseMgt.Startup))]
+namespace MultiwarehouseMgt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
